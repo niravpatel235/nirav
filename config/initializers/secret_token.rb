5c8f448ea88cc4b9ee723e75dc73c9ae2a316412
tt-06 <tt-06@tt-06.(none)>
2013-01-18 15:28:46 +0530

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nirav::Application.config.secret_token = '644c25dc5521e72ae6ae3043057a29fb98f827cf95b17e3bdc1f879a5c23ca039658e59d7431152df2bb8b4cc268113593a52241e0bca434f949b95c4370fa6e'
